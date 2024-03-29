5f1134c92c152264c6d124aacb3da73954df113f
pranithbunny8008 <113341538+pranithbunny8008@users.noreply.github.com>
2024-03-29 23:42:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-05295b6e6c790593e"
+    ami = "ami-0c101f26f147fa7fd"
     instance_type = "t2.micro"
     tags = {
       Name = "dev-ec2"
