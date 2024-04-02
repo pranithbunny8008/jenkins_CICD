c8478a4419d40936f8e9bd7972491cb553090bc8
pranithbunny8008 <113341538+pranithbunny8008@users.noreply.github.com>
2024-04-02 12:41:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-0c101f26f147fa7fd"
     instance_type = "t2.micro"
+    key_name = "useast_1"
     tags = {
       Name = "dev-ec2"
     }
